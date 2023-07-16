de6e6d3552fb7adcb3a3a87b5075977906db2259
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-07-16 22:13:08 +0530

    methods

---
+++ JavaScript/Part 3/Methods.js
@@ -0,0 +1,28 @@
+//Trim
+let msg = "    hi     ";
+msg.trim();
+console.log(msg.trim()); 
+let pass = prompt("enter your password");
+console.log(pass.trim());
+
+//toUpperCase & toLowerCase
+let name = "shashi";
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+
+//indexOf
+let name3 = "shashi";
+console.log(name3.indexOf("i"));
+
+//Method Chaining
+let name1 = "    shashi    ";
+console.log(name1.trim().toUpperCase());
+
+//Slice 
+let name2 = "Shashi Srivastava";
+console.log(name2.slice(0,7));
+console.log(name2.slice(7));
+console.log(name2.length);
+console.log(name2.slice(-11));
+
+//
