edc3f447567122da319415cd4877305130981dad
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-07-17 23:07:19 +0530

    dsa

---
+++ JavaScript/Part 3/Array DSA Method.js
@@ -0,0 +1,12 @@
+//Push
+let array = ["shashi","ravi","sonu"];
+array.push("ram");
+
+//Pop
+array.pop();
+
+//UnShift - To add element in front
+array.unshift("Ram");
+
+//Shift - end
+array.shift("Ram");
