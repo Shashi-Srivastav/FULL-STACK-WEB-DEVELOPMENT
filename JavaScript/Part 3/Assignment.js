629a8dc995f042b621195cb39824c22eb747215d
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-07-20 20:32:03 +0530

    completed

---
+++ JavaScript/Part 3/Assignment.js
@@ -0,0 +1,13 @@
+let arr = [1,2,3,4];
+let n = 3;
+let arrnew = arr.slice(0,n);
+//q1
+console.log(arrnew);
+let newarr = arr.slice(arr.lenth-n);
+//q2
+console.log(newarr);
+//q3
+let string = {};
+if(string.length == 0){
+    console.log("empty");
+}
