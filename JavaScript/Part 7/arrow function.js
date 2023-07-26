82873fbad2af11e26db6f4ed9695f46008cd4a94
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-07-26 22:27:33 +0530

    arrow fx

---
+++ JavaScript/Part 7/arrow function.js
@@ -0,0 +1,22 @@
+//nameless function
+const sum = () => {// here sum is variable as well as function
+    console.log(a+b);
+};
+sum(2,3);
+//5
+
+const cube = (n) => {
+    return n*n*n;
+}
+cube(2);
+//8
+
+const empty = () => {
+    console.log("empty");
+}
+empty();
+//empty
+
+//Emplicit Return --------------------------------------------------------------------------------------------------------------------------
+// or Automatic Return
+
