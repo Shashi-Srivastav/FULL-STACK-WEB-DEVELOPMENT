4664968b6945b44d25741f13d0bce6fce98009b8
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-07-27 21:00:29 +0530

    fx

---
+++ JavaScript/Part 7/set interval function.js
@@ -0,0 +1,7 @@
+// setInterval(function,timeout);
+let id = console.log("set interval");
+setInterval(() => {
+    console.log("shashi");
+}, 2000);
+// clearInterval(id); it will stop function 
+clearInterval(id);
