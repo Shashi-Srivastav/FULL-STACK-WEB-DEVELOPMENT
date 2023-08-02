d5fbe289bf95d2e5047cdd34fad243f74c8c6859
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-08-02 22:07:04 +0530

    map fx

--- JavaScript/Part 8/Map & Filter Function.js
+++ JavaScript/Part 8/Map & Filter Function.js
@@ -1,7 +1,13 @@
 // Map Function----------------------------------------------------------------------------------
-let num = [1,2,3,4,5];
+// let num = [1,2,3,4,5];
 
-let double = num.map((i) => {
-    console.log(i*2);
+// let double = num.map((i) => {
+//     console.log(i*2);
+// });
+// double(num);
+
+//Filter Function --------------------------------------------------------------------------------
+let nums = [1,2,31,3,4,53,23,435,6];
+let result = nums.filter((i)=>{
+    return i%2 == 0;// return even only with new array
 });
-double(num);
