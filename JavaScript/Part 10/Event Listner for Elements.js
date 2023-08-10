eaf876ca47332f73553dc70bf463cf89fc4ab0c7
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-08-10 14:55:20 +0530

    on element

---
+++ JavaScript/Part 10/Event Listner for Elements.js
@@ -0,0 +1,8 @@
+let p = document.querySelector("p");
+p.addEventListener("click", function(){
+    console.log("para is clicked!");
+});
+let div = document.querySelector("div");
+div.addEventListener("mouseenter", function(){
+    console.log("mouse inside box");
+})
