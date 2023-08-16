f4277d0e11e4f1ea655ad494b1d065387c24f063
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-08-16 21:10:03 +0530

    threading

---
+++ JavaScript/Part 11/Js is Single Threaded.js
@@ -0,0 +1,12 @@
+// let a = 12;
+// console.log(a);
+// let b= 33;
+// console.log(b);
+// console.log(a + b);
+
+setTimeout(() => {
+    console.log("hi");
+}, 2000);
+//browser will handle fx timeout
+
+console.log("hlo");
