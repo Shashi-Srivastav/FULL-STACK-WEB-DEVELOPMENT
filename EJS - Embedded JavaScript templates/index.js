80c023d762543b7dc28ed12494833c65724f8b21
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-09-07 21:38:50 +0530

    render

--- EJS - Embedded JavaScript templates/index.js
+++ EJS - Embedded JavaScript templates/index.js
@@ -8,11 +8,16 @@ let port = 8080;
 app.set("views", path.join(__dirname,"/views"));//setting custom path
 app.set("view engine", "ejs");
 
-app.get("/",(req,res)=>{
+// app.get("/",(req,res)=>{
     // res.send("this is home");
     //ejs use render to send
-    res.render("home.ejs");//it will auto look for view folder if we run nodemon index.js from anywhere this will thow error
+   // res.render("home.ejs");//it will auto look for view folder if we run nodemon index.js from anywhere this will thow error
     //to fix this we use manul path set - we have to require path
+// });
+
+// Passing Data to EJS 
+app.get("/roleDice",(req, res)=>{
+    res.render("roleDice.ejs");
 });
 
 app.listen(port, ()=>{
